b30b1cb348a4a44675efad8e1f61bc74d1a623d1
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2016-06-19 15:21:06 +0200

    Switch from python to bash script. - write to tmp instead of location.

--- ncmpcpp/on_song_change.sh
+++ ncmpcpp/on_song_change.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-exec /usr/bin/python /home/vigneshk/projects/scripts/mpd_scrobbler.py
+ARTIST=`mpc | head -1 | sed -E 's/(.*)-(.*)/\1/'`
+TRACK=`mpc | head -1 | sed -E 's/(.*)-(.*)/\2/'`
+
+echo $ARTIST >> /tmp/play_artist.txt
+echo $TRACK >> /tmp/play_track.txt
